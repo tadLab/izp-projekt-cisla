e31ab938c8a5405104e54b533367a49d04144e67
tadLab <tadeas.novo@gmail.com>
2024-10-12 11:09:10 +0200

    Match found +Not found

--- tnine.c
+++ tnine.c
@@ -1,42 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-//Program has to handle 42 contacts that has 2 lines and maximum of 100 characters
+// Program has to handle 42 contacts that have 2 lines and a maximum of 100 characters each
 #define MAX_LENGTH 8400
 
-int main(int argc, char *argv[]) {
+int stringLength(const char* userNumber){
 
-    int userNumber;
+    int userNumberLength = 0;
+
+    while (userNumber[userNumberLength] != '\0') {
+        userNumberLength++;
+    }
+}
+
+int findNumber(const char* userNumber, char line[MAX_LENGTH+1]) {
+    int lineCount = 0;
+    int index = 0;
+    int userNumberLength = 0;
+
+    userNumberLength = stringLength(userNumber);
+
     int character;
+    while ((character = getchar()) != EOF) {
+        if (character == '\n') {
+            line[index] = '\0';
+            lineCount++;
+
+            int found = 0;
+            for (int i = 0; i <= index - userNumberLength; i++) {
+                int match = 1;
+                for (int j = 0; j < userNumberLength; j++) {
+                    if (line[i + j] != userNumber[j]) {
+                        match = 0;
+                        break;
+                    }
+                }
+                if (match) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            if (found) {
+                return lineCount;
+            }
+
+            index = 0;
+        } else {
+            if (index < MAX_LENGTH) {
+                line[index++] = character;
+            }
+        }
+    }
+
+    printf("Not found.\n");
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
     char line[MAX_LENGTH+1];
+    int character;
     int index = 0;
 
-    switch (argc)
-    {
-    case (2):
+    switch (argc) {
+        case 2: {
+            const char* userNumber = argv[1];
+            int foundMatchLine = findNumber(userNumber, line);
 
-        userNumber = atoi(argv[1]);
-
-        printf("Argument that has to be found in the text document is %d\n", userNumber);
-
-        break;
-    case (1):
-        while((character = getchar()) != EOF){
-            line[index++] = character;
+            if (foundMatchLine > 0) {
+                printf("Match found on line number %d\n", foundMatchLine);
+            }
+            break;
         }
+        case 1: {
+            while ((character = getchar()) != EOF) {
+                if (index < MAX_LENGTH) {
+                    line[index++] = character;
+                } else {
+                    break;
+                }
+            }
 
-        line[index] = '\0';
+            line[index] = '\0';
 
-        printf("%s", line);
-        printf("\n");
+            printf("%s", line);
+            printf("\n");
 
-        break;
-    default:
-
-        printf("Too many arguments provided! Try again and put one or two only!\n");
-
-        break;
+            break;
+        }
+        default:
+            printf("Too many arguments provided! Try again and put one or two only!\n");
+            break;
     }
 
     return 0;
