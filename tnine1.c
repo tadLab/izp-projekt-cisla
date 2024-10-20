69335278c24013f0e932586ccbfd5beba5b5d82b
tadLab <tadeas.novo@gmail.com>
2024-10-20 14:50:09 +0200

    projv2.3

--- tnine1.c
+++ tnine1.c
@@ -7,6 +7,21 @@
 #define MAX_CONTACTS 42     //The maximum number of contacts
 #define MAX_INFO 2          //The number of pieces of information stored per contact (name, number)
 
+//EVERY NUMBER REPRESENTS THREE LETTERS IN ALPHABET
+const char* getLetters(char digit) {
+    switch (digit) {
+        case '2': return "abc";
+        case '3': return "def";
+        case '4': return "ghi";
+        case '5': return "jkl";
+        case '6': return "mno";
+        case '7': return "pqrs";
+        case '8': return "tuv";
+        case '9': return "wxyz";
+        case '0': return "+";
+    }
+}
+
 //FINDS A SUBSTRING IF PRESENT
 bool isSubstring(const char* substring, char* string) {
     //Loops through all characters in the provided string
@@ -31,7 +46,6 @@ bool isSubstring(const char* substring, char* string) {
     }
 
     return false;
-
 }
 
 //FINDS THE RELATIVE CONTACT BASED ON THE USER NUMBER
@@ -75,7 +89,7 @@ int main(int argc, char const *argv[]) {
     int character;
     int column = 0;
 
-    //Loops through each character until it reaches EOF - End Of File
+    //Storig all contacts with loop that goes through each character until it reaches EOF - End Of File
     while ((character = getchar()) != EOF && row < MAX_CONTACTS) {
         if (character != '\n') {
             //Inserts the character into the correct column: 0 - name, 1 - tel. number
@@ -98,9 +112,30 @@ int main(int argc, char const *argv[]) {
     case 2: {
         //userNumber stores given number on stdin
         const char* userNumber = argv[1];
+        //letters stores characaters corresponding to number given on stdin
+        char letters[strlen(userNumber)+1][3];
 
         if(isValid(userNumber)){
-            findContact(userNumber, contacts);
+            //findContact(userNumber, contacts);
+            for(int i = 0; userNumber[i] != '\0'; i++){
+               const char* corespondingLetters = getLetters(userNumber[i]);
+               for(int j = 0; corespondingLetters[j] != '\0'; j++){
+                //correspondingLetters are stored in the letters one by one
+                letters[i][j] = corespondingLetters[j];
+               }
+               
+            }
+
+            //Prints corresponding letters for given number
+            for(int i = 0; i < strlen(userNumber); i++){
+                printf("Letters for %c: ", userNumber[i]);
+            for(int j = 0; j < 3 && letters[i][j] != '\0'; j++){
+                printf("%c ", letters[i][j]);
+            }
+
+                printf("\n");
+            }
+            
         } else{
             printf("Error occurred: Please provide valid input on stdin (0-9).\n");
         }
